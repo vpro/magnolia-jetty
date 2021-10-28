4e6d8da89584a9db5fbce97c981bbe388c3d3238
Michiel Meeuwissen <git@meeuw.org>
2021-10-28 13:35:35 +0200

    MGNL-18589 This fixes reloading of css and js too.

--- src/main/java/nl/vpro/jetty/MagnoliaWebAppClassLoader.java
+++ src/main/java/nl/vpro/jetty/MagnoliaWebAppClassLoader.java
@@ -1,11 +1,15 @@
 package nl.vpro.jetty;
 
-import java.io.*;
-import java.net.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.*;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-
 import org.eclipse.jetty.webapp.WebAppClassLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,14 +34,19 @@ public class MagnoliaWebAppClassLoader extends WebAppClassLoader {
     private final File[] dirs;
 
     private final Map<String, Resource> found = new ConcurrentHashMap<>();
+    private final WatchService watchService = FileSystems.getDefault().newWatchService();
+
 
     {
         File dir = new File(System.getProperty("user.dir")).getParentFile();
-        dirs = dir.listFiles(pathname -> pathname.isDirectory() && new File(pathname, RESOURCES).isDirectory());
-        if (dirs != null) {
+        dirs = Arrays.stream(Objects.requireNonNull(dir.listFiles(pathname -> pathname.isDirectory() && new File(pathname, RESOURCES).isDirectory()))).map(f -> new File(f, RESOURCES)).toArray(File[]::new);
+        if (dirs.length > 0) {
             LOG.info("Watching files in {}", Arrays.asList(dirs));
         } else {
             LOG.info("Could not find files to watch");
+        }
+        if (dirs.length > 0) {
+            watch();
         }
     }
 
@@ -50,9 +59,8 @@ public class MagnoliaWebAppClassLoader extends WebAppClassLoader {
     }
 
     @Override
-    public URL getResource(String name) {
-        if (! name.endsWith(".class")) { // no chance on that
-            String fileName = RESOURCES + name;
+    public URL getResource(String fileName) {
+        if (! fileName.endsWith(".class")) { // no chance on that
             for (File file : dirs) {
                 final File resource = new File(file, fileName);
                 if (resource.canRead()) {
@@ -72,7 +80,7 @@ public class MagnoliaWebAppClassLoader extends WebAppClassLoader {
 
                     try {
                         LOG.debug("Found {}", resource);
-                        return getUrl(resource);
+                        return resource.toURI().toURL();
                     } catch (MalformedURLException e) {
                         LOG.error("{} -> {}", resource, e.getMessage());
 
@@ -83,7 +91,7 @@ public class MagnoliaWebAppClassLoader extends WebAppClassLoader {
             }
             LOG.debug("Cannot read {}", fileName);
         }
-        return super.getResource(name);
+        return super.getResource(fileName);
     }
 
     private URL[] urls;
@@ -96,10 +104,10 @@ public class MagnoliaWebAppClassLoader extends WebAppClassLoader {
     public synchronized URL[] getURLs() {
         if (urls == null) {
             List<URL> result = new ArrayList<>();
-            for (File f : dirs) {
-                File resourceFile = new File(f, RESOURCES);
+            for (File resourceFile : dirs) {
                 try {
-                    result.add(getUrl(resourceFile));
+                    URL url = resourceFile.toURI().toURL();
+                    result.add(url);
                 } catch (MalformedURLException e) {
                     LOG.error("{} -> {}", resourceFile, e.getMessage());
                 }
@@ -111,18 +119,6 @@ public class MagnoliaWebAppClassLoader extends WebAppClassLoader {
         return urls;
     }
 
-    URL getUrl(File f) throws MalformedURLException {
-        return f.toURI().toURL();
-        // Magnolia get thoroughly confused if you give it any then else then file: or jar-file:
-        /*
-        return new URL("files", null,  -1, f.getAbsolutePath(), new URLStreamHandler() {
-            @Override
-            protected URLConnection openConnection(URL u) throws IOException {
-                return new File(u.getPath()).toURI().toURL().openConnection();
-            }
-        });
-         */
-    }
 
     /**
      * Just represents one file with its original last modified timestamp.
@@ -137,4 +133,89 @@ public class MagnoliaWebAppClassLoader extends WebAppClassLoader {
             lastModified =  f.lastModified();
         }
     }
+
+
+    /**
+     * Watch for all creation events, and if one happens, touch the containing directory.
+     * This triggers info.magnolia.resourceloader.classpath.service.impl.devmode.DevelopmentModeClasspathService.reload
+     *
+     * Otherwise it doesn't. It seems a bug?
+     */
+    protected  void watch() {
+        new Thread(() -> {
+            final Map<WatchKey, Path> keys = registerWatchers();
+            LOG.info("Watching {} directories for new files", keys.size());
+            while (true) {
+                try {
+                    WatchKey key = watchService.take();
+                    final Path dir = keys.get(key);
+                    if (dir == null) {
+                        continue;
+                    }
+                    for (WatchEvent<?> event : key.pollEvents()) {
+                        Path context = dir.resolve((Path) event.context());
+                        if (Files.isRegularFile(context)) {
+                            for (File d : dirs) {
+                                if (context.toAbsolutePath().startsWith(d.getAbsolutePath())) {
+                                    LOG.info("Found change in {}, touching {}", context, d);
+                                    long lastModified = System.currentTimeMillis();
+                                    if (d.lastModified() < lastModified) {
+                                        boolean success = d.setLastModified(lastModified);
+                                        if (!success) {
+                                            LOG.warn("Could not set timestamp of {}", d);
+                                        }
+                                    }
+                                }
+                            }
+                        } else if (Files.isDirectory(context)) {
+                            for (File d : dirs) {
+                                if (context.toAbsolutePath().startsWith(d.getAbsolutePath())) {
+                                    LOG.info("Found new directory in {} in {}, watching too", context, d);
+                                    try {
+                                        context.register(watchService, ENTRY_CREATE);
+                                    } catch (IOException e) {
+                                        LOG.error(e.getMessage());
+                                    }
+                                    break;
+                                }
+                            }
+                        }
+
+                    }
+                    boolean valid = key.reset(); // IMPORTANT: The key must be reset after processed
+                    if (!valid) {
+                        break;
+                    }
+                } catch (InterruptedException e) {
+                    LOG.info("Interrupted");
+                    //return;
+                }
+
+            }
+
+        }).start();
+    }
+
+    protected Map<WatchKey, Path>  registerWatchers()  {
+        final Map<WatchKey, Path> keys = new HashMap<>();
+
+         for (File watchedDirectory : dirs) {
+            Path path = Paths.get(watchedDirectory.toString());
+            try {
+                Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                        WatchKey key = dir.register(watchService, ENTRY_CREATE);
+                        LOG.debug("Watching {}", dir.toAbsolutePath());
+                        keys.put(key, dir);
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+            } catch (IOException e) {
+                LOG.warn("For {}: {}", watchedDirectory, e.getMessage());
+            }
+         }
+         return keys;
+    }
+
 }
